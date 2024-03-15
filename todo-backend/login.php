46ea29938a05ad1a740a1405660d6b2a0fbeec1b
mohammad mansour <mhmd.mans.2001@gmail.com>
2024-03-15 12:55:58 +0200

    feat(signup): signup and login apis done

--- todo-backend/login.php
+++ todo-backend/login.php
@@ -1,31 +1,61 @@
 <?php
 include("connection.php");
 
-$username = $_POST["email"];
+$identifier = $_POST["identifier"];
 $password = $_POST["password"];
 
-$check_user = $mysqli->prepare('select id,email,password,name from users where email=?');
-$check_user->bind_param('s', $email);
-$check_user->execute();
-$check_user->store_result();
-$user_exists = $check_user->num_rows();
+if (filter_var($identifier, FILTER_VALIDATE_EMAIL)) {
+    $query = $mysqli->prepare("SELECT user_id, username, email, password FROM users WHERE email = ?");
+} else {
+    $query = $mysqli->prepare("SELECT user_id, username, email, password FROM users WHERE username = ?");
+}
 
-$response = array();
+$query->bind_param('s', $identifier);
+$query->execute();
+$query->store_result();
 
-if ($user_exists > 0) {
-    $check_user->bind_result($user_id, $username, $hashed_password);
-    $check_user->fetch();
+$query->bind_result($user_id, $username, $email, $hashed_password);
+$query->fetch();
+
+$num_rows = $query->num_rows;
+
+$response = [];
+
+if ($num_rows == 0) {
+    $response['status'] = "user not found";
+} else {
     if (password_verify($password, $hashed_password)) {
         $response['status'] = "success";
         $response['message'] = "Login successful";
         $response['user_id'] = $user_id;
+        $response["username"] = $username;
+        $response["email"] = $email;
+        $task_query = $mysqli->prepare('select task_id, user_id, task, task_check from todos where user_id = ?');
+        $task_query->bind_param('i', $user_id);
+        $task_query->execute();
+        $task_query->store_result();
+
+        $num_rows = $task_query->num_rows;
+
+        if ($num_rows == 0) {
+            $response['todos'] = [];
+        } else {
+            $todos = [];
+            $task_query->bind_result($task_id, $user_id, $task, $task_check);
+            while ($task_query->fetch()) {
+                $todo = [
+                    'id' => $task_id,
+                    'user_id' => $user_id,
+                    'task' => $task,
+                    'task_check' => $task_check
+                ];
+                $todos[] = $todo;
+            }
+            $response['todos'] = $todos;
+        }
     } else {
-        $response['status'] = "error";
-        $response['message'] = "Incorrect password";
+        $response['status'] = "incorrect credentials";
     }
-} else {
-    $response['status'] = "error";
-    $response['message'] = "User not found";
 }
 
 echo json_encode($response);
--- todo-backend/signup.php
+++ todo-backend/signup.php
@@ -6,16 +6,17 @@ $password = $_POST["password"];
 $email = $_POST["email"];
 
 $check_user = $mysqli -> prepare('select * from users where username = ? or email = ?');
-$check_user -> bind_param('ss', $username, $password, $email);
+$check_user -> bind_param('ss', $username, $email);
 $check_user -> execute();
 $check_user -> store_result();
 $user_exists = $check_user -> num_rows();
 
 if ($user_exists == 0) {
     $hashed_password = password_hash($password, PASSWORD_BCRYPT);
-    $user_exists = $mysqli -> prepare('insert into users(user_id,username,email) value(null,?,?,?)');
-    $query -> bind_param('sss', $username, $email, $hashed_password);
-    $user_exists -> execute();
+    $insert_user = $mysqli -> prepare('insert into users(user_id,username,email,password) value(null,?,?,?)');
+    $insert_user -> bind_param('sss', $username, $email, $hashed_password);
+    $insert_user -> execute();
+
     $response['status'] = "success";
     $response['message'] = "user $username was created";
     
