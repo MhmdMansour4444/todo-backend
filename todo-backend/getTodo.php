8a1e49a87d2db828c9af3667972c6b7feaf86600
mohammad mansour <mhmd.mans.2001@gmail.com>
2024-03-15 13:11:55 +0200

    chores(fixes):syntax and typos fixes

--- todo-backend/getTodo.php
+++ todo-backend/getTodo.php
@@ -5,27 +5,27 @@ $response = array();
 
 $task_id = $_POST['task_id'];
 
-$select_query = $mysqli->prepare('select * from todos where id = ?');
+$select_query = $mysqli->prepare('select * from todos where task_id = ?');
 $select_query->bind_param('i', $task_id);
 $select_query->execute();
 $select_query->store_result();
 
 if ($select_query->num_rows > 0) {
-    $select_query->bind_result($id, $task, $task_check);
+    $select_query->bind_result($task_id, $task, $task_check);
     $select_query->fetch();
 
-    $todo_item = array(
-        'id' => $id,
+    $task_item = array(
+        'task_id' => $task_id,
         'task' => $task,
         'task_check' => $task_check
     );
 
     $response['status'] = "success";
-    $response['message'] = "Todo item retrieved successfully";
-    $response['todo_item'] = $todo_item;
+    $response['message'] = "Task item retrieved successfully";
+    $response['todo_item'] = $task_item;
 } else {
     $response['status'] = "error";
-    $response['message'] = "Todo item not found";
+    $response['message'] = "Task item not found";
 }
 
 echo json_encode($response);
